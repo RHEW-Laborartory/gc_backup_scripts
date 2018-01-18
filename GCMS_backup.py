38bfec8242f2036508bc48014af34c7644cdd022
LawerenceLee <lucifitz.edward@gmail.com>
2018-01-18 13:18:18 -0800

    initial commit

---
+++ GCMS_backup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import datetime
+import os
+
+
+def date():
+    """Uses datetime library to find todays date, and
+    returns the string version."""
+    today = datetime.dateime.now()
+    return datetime.strftime("%y%m%d", today) 
+
+
+def backup(date):
+    """Python runs hidden shell prompt with the specified
+    rsync command."""
+    try:
+        os.system("rsync -vaP /home/rob/halo1/ /media/rob/22C622FFC622D2B9/gcms_most_recent_backup")
+    except Exception as e:
+        print(e)
+
+
+def main():
+    backup(date)
+
+
+if __name__ == "__main__":
+    os.system("cd /media/rob/22C622FFC622D2B9/ && mkdir gcms_most_recent_backup")
+    main()
